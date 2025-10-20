2186ffa2109766dcd5ee3c2275756873cd891212
gwatch <gwatch@example.com>
2025-10-20 07:59:57 +0200

    gwatch auto-commit

--- onboarding/js/site.js
+++ onboarding/js/site.js
@@ -747,7 +747,7 @@ if (scopeData.services && scopeData.services.length > 0) {
         Object.keys(groupedSettings).forEach(groupName => {
             html += `<div class="accordion-item">
                             <div class="accordion-header">
-                                <div class="accordion-button accordion-button-sm collapsed" data-bs-toggle="collapse" aria-expanded="false">
+                                <div class="accordion-button accordion-button-sm collapsed" data-bs-toggle="collapse" aria-expanded="false" data-bs-target="${safeReplace(groupName.toLowerCase(), ' ', '_')}">
                                     <div type="button" class="w-100 collapsed" data-bs-toggle="collapse" data-bs-target="${safeReplace(groupName.toLowerCase(), ' ', '_')}" aria-expanded="false">${groupName}</div>
                                 </div>
                             </div>
