2a2e8fa4488d85645b2126c5d945b9bb19a01252
gwatch <gwatch@example.com>
2025-10-24 14:20:45 +0200

    gwatch auto-commit

--- onboarding/js/site.js
+++ onboarding/js/site.js
@@ -633,24 +633,25 @@ if (scopeData.services && scopeData.services.length > 0) {
                                     class="accordion-collapse collapse" 
                                     ignore-data-bs-parent="#${scope}-service-settings">
                                     <div class="accordion-body">
-                                        ${service.allowOn}
                                     <div class="container-fluid">
                                         <div class="row mb-2">
                                             <div class="col-12">
+                                            ${service.allowOn === 'undefined' || service.allowOn.some((x) => x.toLowerCase() === 'integrator') ? `
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input integrator-service" type="checkbox" role="link-service-to-entity" id="${scope}-${safeRename(service.name)}-integrator" data-service="${service.name}" data-entity="integrator">
                                                     <label class="form-check-label form-check-label-sm" for="${scope}-${safeRename(service.name)}-integrator">
                                                         Link to Integrator
                                                         <i class="bi bi-info-circle setting-info text-info" data-bs-toggle="tooltip" data-bs-title="Link ${service.description} to Integrator"></i>
                                                     </label>
-                                                </div>
+                                                </div>` : ''}
+                                            ${service.allowOn === 'undefined' || service.allowOn.some((x) => x.toLowerCase() === 'deviceuser') ? `
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input deviceuser-service" type="checkbox" role="link-service-to-entity" id="${scope}-${safeRename(service.name)}-deviceuser" data-service="${service.name}" data-entity="deviceuser">
                                                     <label class="form-check-label form-check-label-sm" for="${scope}-${safeRename(service.name)}-deviceuser">
                                                         Link to Device User
                                                         <i class="bi bi-info-circle setting-info text-info" data-bs-toggle="tooltip" data-bs-title="Link ${service.description} to Device User"></i>
                                                     </label>
-                                                </div>
+                                                </div>` : ''}
                                             </div>
                                         </div>
                                     </div>
