8b3262b66d730890032aade3e9a7ef002a19fe8e
gwatch <gwatch@example.com>
2025-10-24 14:58:01 +0200

    gwatch auto-commit

--- onboarding/js/site.js
+++ onboarding/js/site.js
@@ -636,7 +636,7 @@ if (scopeData.services && scopeData.services.length > 0) {
                                     <div class="container-fluid">
                                         <div class="row mb-2">
                                             <div class="col-12">
-                                            ${(scopeData.allowOn === 'undefined' || (scopeData.allowOn || []).some((x) => x.toLowerCase() === 'integrator')) &&
+                                            ${(scopeData.allowOn === 'undefined' || (scopeData.allowOn || []).some((x) => x.toLowerCase() === 'integrator')) ||
                                                 (service.allowOn === 'undefined' || (service.allowOn || []).some((x) => x.toLowerCase() === 'integrator')) ? `
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input integrator-service" type="checkbox" role="link-service-to-entity" id="${scope}-${safeRename(service.name)}-integrator" data-service="${service.name}" data-entity="integrator">
@@ -645,7 +645,7 @@ if (scopeData.services && scopeData.services.length > 0) {
                                                         <i class="bi bi-info-circle setting-info text-info" data-bs-toggle="tooltip" data-bs-title="Link ${service.description} to Integrator"></i>
                                                     </label>
                                                 </div>` : ''}
-                                            ${(scopeData.allowOn === 'undefined' || (scopeData.allowOn || []).some((x) => x.toLowerCase() === 'deviceuser')) &&
+                                            ${(scopeData.allowOn === 'undefined' || (scopeData.allowOn || []).some((x) => x.toLowerCase() === 'deviceuser')) ||
                                                 (service.allowOn === 'undefined' || (service.allowOn || []).some((x) => x.toLowerCase() === 'deviceuser')) ? `
                                                 <div class="form-check form-switch">
                                                     <input class="form-check-input deviceuser-service" type="checkbox" role="link-service-to-entity" id="${scope}-${safeRename(service.name)}-deviceuser" data-service="${service.name}" data-entity="deviceuser">
